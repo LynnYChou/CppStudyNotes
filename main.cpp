2438df2fda889657650e4bac0493fac1878882fe
theIdealist <ychou@fandm.edu>
2020-02-04 16:46:52 -0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,18 @@
+/* Notes for understanding data type and variables */
+
+#include <isostream>
+using namespace std;
+
+int main(){
+  
+  int age {10}; // initialize an integer variable with value 10
+  // same as int age = 10
+  
+  signed x; // keep both positive and negative integers (this is the default state for data type "int")
+  unsigned y; // keep only the positive integers
+  
+  // below are ways to initialize real numbers
+  float a;
+  double b;
+  long c;
+  
