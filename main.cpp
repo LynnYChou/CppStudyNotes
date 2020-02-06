e33d6bdd40047c722753d4768e97c72ff26611c2
theIdealist <ychou@fandm.edu>
2020-02-05 22:26:32 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,18 @@
 
+// Notes about learning C++ programming from the beginning to beyond
+
 #include <iostream>
 #include <typeinfo>
 
 using namespace std;
 
 int main(){
-  
-  /* Notes for understanding data types and variables in C++ programming */
+
+/*
+  // Understanding data types and variables
 
   int age {10}; // Initialize an integer variable with value 10
-  // Same as int age = 10
+  // Similar to int age = 10;
   
   signed x; // Keep both positive and negative integers (this is the default state for data type "int")
   unsigned y; // Keep only the positive integers
@@ -26,12 +29,12 @@ int main(){
   auto myVar = 9.5; // Let the compiler decide the data type
   cout << "Type of myVar: " << typeid(myVar).name() << endl;
 
-  /* Understanding reference variable */
+  // Understanding reference variable
 
   int x = 10;
   int &v = x; // You MUST initialize a reference variable (to which variable it is referred to)
 
-  /* Understanding arithmetic operations */
+  // Understanding arithmetic operations
   
   int first_oper {20};
   int second_oper {10};
@@ -41,14 +44,14 @@ int main(){
   result = first_oper % second_oper;
   cout << "Your result is :" << result << endl;
 
-  /* Take input from keyboard */
+  // Take input from keyboard
 
   int var {}; // Initialize var with value 0
   cout << "Please enter an integer value: ";
   cin >> var; //Extract the value according to the type of var
   cout << "Var is: " << var << endl;
 
-  /* In this example, the result will be 0, can you see why? */
+  // Error-generating example
   
   int x;
   int y;
@@ -59,15 +62,39 @@ int main(){
 
   // The result of an arithmetic operation is decided by (the highest precedence data type of) the operands 
 
-  /* Solution 1 (by explicit casting) */
+  // Solution (by explicit casting)
 
   int x;
   int y;
   double result;
   result = (double)x / y;
 
+*/
+  // Understand pointers
+
+  int temp1 = 300, temp2 = 351;
+  cout << "Address of temp1: " << &temp1 << endl;
+  cout << "Address of temp2: " << &temp2 << endl;
+  int *pt2 = &temp2;
+  cout << "Content of pt2: " << pt2 << endl;
+  cout << "Content of the location: " << *pt2 << endl; 
+  pt2 += 1;
+  cout << "Content of pt2 now: " << pt2 << endl;
+  cout << "Content of the location now: " << *pt2 << endl; 
+
+  // Convert a character pointer to an integer pointer
+  char *pk = (char *)pt2;
+  cout << "Content of the location that pk is pointed to: " << *pk << endl;
+  // Since pk is a character pointer (which only keep one byte),
+  // it's now pointed to the first address of the value 351 (out of four addresses)
+  // in which contains the value of 44 (in binary). So by ASCII table, the output of *pk is "," 
+
+
+
+
 
   return 0;
+
 }
 
 
